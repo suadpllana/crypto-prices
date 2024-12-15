9e95acc4854c5d243ae1ca32dfadac8f09b654bc
oltapllana <oltapllana@gmail.com>
2024-12-15 04:09:29 +0100

    first commit

--- src/images.js
+++ src/images.js
@@ -1,108 +1,221 @@
 import bitcoin from "./assets/bitcoin.png"
-import etherum from "./assets/etherum.png"
+import ethereum from "./assets/etherum.png"
 import tether from "./assets/tether.png"
-import bnb from "./assets/bnb.png"
+import binancecoin from "./assets/bnb.png"
 import dogecoin from "./assets/dogecoin.png"
 import litecoin from "./assets/litecoin.png"
 import solana from "./assets/solana.png"
-import shiba from "./assets/shiba.png"
+import shibainu from "./assets/shiba.png"
 import steth from "./assets/steth.png"
 import tron from "./assets/tron.png"
-import usdCoin from "./assets/usd-coin.png"
+import usdcoin from "./assets/usd-coin.png"
 import xrp from "./assets/xrp.png"
 import aave from "./assets/aave.png"
 import algorand from "./assets/algorand.png"
 import arweave from "./assets/arweave.png"
 import avalanche from "./assets/avalanche.png"
-import bitcoinSv from "./assets/bitcoin-sv.png"
-import btcCash from "./assets/btc-cash.png"
+import bitcoinsv from "./assets/bitcoin-sv.png"
+import bitcoincash from "./assets/btc-cash.png"
 import cardano from "./assets/cardano.png"
-import chainLink from "./assets/chain-link.png"
+import chainlink from "./assets/chain-link.png"
 import cosmos from "./assets/cosmos.png"
-import cryptoCom from "./assets/crypto.com.png"
-import etherumClassic from "./assets/etherum-classic.png"
+import cryptocomcoin from "./assets/crypto.com.png"
+import ethereumclassic from "./assets/etherum-classic.png"
 import fantom from "./assets/fantom.png"
 import fetch from "./assets/fetch.png"
 import filecoin from "./assets/filecoin.png"
 import flow from "./assets/flow.png"
 import gatetoken from "./assets/gatetoken.png"
-import graph from "./assets/graph.png"
-import hedera from "./assets/hedera.png"
+import thegraph from "./assets/graph.png"
+import hederahashgraph from "./assets/hedera.png"
 import helium from "./assets/helium.png"
-import injectiveProtocol from "./assets/injective-protocol.png"
-import internet from "./assets/internet.png"
+import injectiveprotocol from "./assets/injective-protocol.png"
+import internetcomputer from "./assets/internet.png"
 import kucoin from "./assets/kucoin.png"
-import lidaDao from "./assets/lido-dao.png"
+import lidodao from "./assets/lido-dao.png"
 import maker from "./assets/maker.png"
 import monero from "./assets/monero.png"
-import multiCollateral from "./assets/multi-collateral.png"
-import nearProtocol from "./assets/near-protocol.png"
+import multicollateraldai from "./assets/multi-collateral.png"
+import nearprotocol from "./assets/near-protocol.png"
 import okb from "./assets/okb.png"
 import polkadot from "./assets/polkadot.png"
 import polygon from "./assets/polygon.png"
 import quant from "./assets/quant.png"
 import stacks from "./assets/stacks.png"
 import theta from "./assets/theta.png"
-import thorChain from "./assets/thor-chain.png"
+import thorchain from "./assets/thor-chain.png"
 import uniswap from "./assets/uniswap.png"
-import unusSed from "./assets/unus-sed.png"
+import unussedleo from "./assets/unus-sed.png"
 import vechain from "./assets/vechain.png"
-import wrappedBtc from "./assets/wrapped-btc.png"
+import wrappedbitcoin from "./assets/wrapped-btc.png"
 import stellar from "./assets/stellar.png"
-
-
-
+import sp8de from "./assets/sp8de.png"
+import mantradao from "./assets/mantrodao.png"
+import gala from "./assets/gala.png"
+import thesandbox from "./assets/thesandbox.png"
+import eos from "./assets/eos.png"
+import kucointoken from "./assets/kucointoken.png"
+import raydium from "./assets/raydium.png"
+import tezos from "./assets/tezos.png"
+import curvedaotoken from "./assets/curvedaotoken.png"
+import iota from "./assets/iota.png"
+import neo from "./assets/neo.png"
+import aioznetwork from "./assets/aioznetwork.png"
+import decentraland from "./assets/decentraland.png"
+import axieinfinity from "./assets/axieinfinity.png"
+import elrondegld from "./assets/multiversx.png"
+import chiliz from "./assets/chiliz.png"
+import superfarm from "./assets/superfarm.png"
+import xinfinnetwork from "./assets/xinfinnetwork.png"
+import mina from "./assets/mina.png"
+import zcash from "./assets/zcash.png"
+import pendle from "./assets/pendle.png"
+import confluxnetwork from "./assets/conflux.png"
+import nexo from "./assets/nexo.png"
+import compound from "./assets/compound.png"
+import pancakeswap from "./assets/pancakeswap.png"
+import akashnetwork from "./assets/akashnetwork.png"
+import ftxtoken from "./assets/ftxtoken.png"
+import ecash from "./assets/ecash.png"
+import amp from "./assets/amp.png"
+import oasisnetwork from "./assets/oasis.png"
+import reserverights from "./assets/reserverights.png"
+import gnosisgno from "./assets/gnosis.png"
+import synthetixnetworktoken from "./assets/synthetix.png"
+import oneinch from "./assets/oneinch.png"
+import telcoin from "./assets/telcoin.png"
+import nervosnetwork from "./assets/nervos.png"
+import kava from "./assets/kava.png"
+import kusama from "./assets/kusama.png"
+import livepeer from "./assets/livepeer.png"
+import trustwallettoken from "./assets/trustwallet.png"
+import sushiswap from "./assets/sushiswap.png"
+import dash from "./assets/dash.png"
+import dexe from "./assets/dexe.png"
+import holo from "./assets/holo.png"
+import enjincoin from "./assets/enjincoin.png"
+import thetafuel from "./assets/thetafuel.png"
+import wootrade from "./assets/woo.png"
+import zilliqa from "./assets/zilliqa.png"
+import zerox from "./assets/zeroxprotocol.png"
+import trueusd from "./assets/trueusd.png"
+import nxm from "./assets/nxm.png"
+import harmony from "./assets/harmony.png"
+import origintrail from "./assets/origintail.png"
 export const images = {
+    origintrail,
+    harmony,
+    nxm,
+    trueusd,
+    zerox,
+    zilliqa,
+    wootrade,
+    thetafuel,
+    enjincoin,
+    holo,
+dexe,
+    dash,
+    sushiswap,
+    trustwallettoken,
+    livepeer, 
+    kusama,
+    kava,
+    nervosnetwork,
+    telcoin,
+    oneinch,
+    synthetixnetworktoken,
+    gnosisgno,
+    reserverights,
+    oasisnetwork,
+    amp,
+    ecash,
+    ftxtoken,
+    pancakeswap,
+    akashnetwork,
+    compound,
+    nexo,
+    confluxnetwork,
+    pendle,
+    zcash,
+    mina,
+    xinfinnetwork,
+    superfarm,
+    chiliz,
+    elrondegld,
+    axieinfinity,
+    decentraland,
+    aioznetwork,
+    neo,
+    iota,
+    curvedaotoken,
+    tezos,
+    raydium,
+    kucointoken,
+    eos,
+    thesandbox,
+    gala,
+    mantradao,
+    sp8de,
     bitcoin,
-    etherum,
+    ethereum,
     tether,
-    bnb,
-    dogecoin,
-    litecoin,
-    solana,
-    steth,
-    tron,
-    usdCoin,
+    binancecoin,
     xrp,
+    solana,
+    dogecoin,
+    cardano,
+    cardano,
+    tron,
+    avalanche,
+    chainlink,
+    shibainu,
+    litecoin,
+wrappedbitcoin,
+    steth,
+   
+    usdcoin,
+    
     aave,
     algorand,
     arweave,
-    avalanche,
-    bitcoinSv,
-    btcCash,
+  
+    bitcoinsv,
+    bitcoincash,
     cardano,
-    chainLink,
+ 
     cosmos,
-    cryptoCom,
-    etherumClassic,
+    cryptocomcoin,
+    ethereumclassic,
     fantom,
     fetch,
     filecoin,
     flow,
     gatetoken,
-    graph,
-    hedera,
+    thegraph,
+    hederahashgraph,
     helium,
-    injectiveProtocol,
-    internet,
+    injectiveprotocol,
+    internetcomputer,
     kucoin,
-    lidaDao,
+    lidodao,
     maker,
     monero,
-    multiCollateral,
-    nearProtocol,
+    multicollateraldai,
+    nearprotocol,
     okb,
     polkadot,
     polygon,
     quant,
-    shiba,
+   
     stacks,
     theta,
-    thorChain,
+    thorchain,
     uniswap,
-    unusSed,
+    unussedleo,
     vechain,
-    wrappedBtc,
+  
     stellar
 
+
 }
+   
